0f32d735cce9e64e3c1f50a04c42ea595d3eafc1
KevinCranmer <cranmerkevin39@gmail.com>
2023-10-07 17:37:50 -0500

    Handle situation where another plugin overrides the respawn event. We will override that override.

--- src/main/java/me/crazycranberry/vanillahungergames/commands/JoinHungerGamesCommand.java
+++ src/main/java/me/crazycranberry/vanillahungergames/commands/JoinHungerGamesCommand.java
@@ -13,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 
 import static me.crazycranberry.vanillahungergames.managers.HungerGamesWorldManager.hungerGamesWorld;
 import static me.crazycranberry.vanillahungergames.managers.HungerGamesWorldManager.isWorldReady;
+import static me.crazycranberry.vanillahungergames.utils.StartingWorldConfigUtils.startingWorldConfigExists;
 
 public class JoinHungerGamesCommand implements CommandExecutor {
     @Override
@@ -24,10 +25,19 @@ public class JoinHungerGamesCommand implements CommandExecutor {
                 p.sendMessage("There is not a hunger games tournament happening at the moment.");
             } else if (p.getWorld().equals(hgWorld)) {
                 p.sendMessage("You're already in the hunger games tournament, you silly goose.");
+            } else if (startingWorldConfigExists(p) && (args.length < 1 || (!args[0].equals("keep") && !args[0].equals("overwrite")))) {
+                p.sendMessage("Your inventory/location of when you last joined the hunger games is still saved. Please type one of the following:");
+                p.sendMessage("\"/hgjoin keep\" - This will keep your previous inventory/location that you had when you last joined the hunger games.");
+                p.sendMessage("\"/hgjoin overwrite\" - This will overwrite and your current inventory/location will be saved. (Note: OLD ITEMS MIGHT BE LOST FOREVER)");
+            } else if (startingWorldConfigExists(p) && args[0].equals("keep")) {
+                p.sendMessage("Joining the hunger games... And keeping your previous inventory/location as your return configuration.");
+                Bukkit.getPluginManager().callEvent(new ParticipantAttemptToJoinEvent(new Participant(p), false));
+            } else if (startingWorldConfigExists(p) && args[0].equals("overwrite")) {
+                p.sendMessage("Joining the hunger games... And overwriting your old inventory/location with your current one.");
+                Bukkit.getPluginManager().callEvent(new ParticipantAttemptToJoinEvent(new Participant(p), true));
             } else {
                 p.sendMessage("Joining the hunger games...");
-                Bukkit.getPluginManager().callEvent(new ParticipantAttemptToJoinEvent(new Participant(p)));
-                return false;
+                Bukkit.getPluginManager().callEvent(new ParticipantAttemptToJoinEvent(new Participant(p), true));
             }
         }
         return true;
