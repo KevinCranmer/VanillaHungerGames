0f25d5409924b2718caeb60e04f3aefe40b328e5
Kevin Cranmer <51764175+KevinCranmer@users.noreply.github.com>
2023-11-30 15:13:48 -0600

    PreGame Countdown Timer and Min Num Players implemented (#16)

--- src/main/java/me/crazycranberry/vanillahungergames/HungerGamesEventList.java
+++ src/main/java/me/crazycranberry/vanillahungergames/HungerGamesEventList.java
@@ -7,8 +7,10 @@ import me.crazycranberry.vanillahungergames.events.InvincibilityEndedEvent;
 import me.crazycranberry.vanillahungergames.events.TournamentStartedEvent;
 import org.bukkit.ChatColor;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static me.crazycranberry.vanillahungergames.VanillaHungerGames.getPlugin;
 import static me.crazycranberry.vanillahungergames.managers.HungerGamesWorldManager.borderBoundaries;
 
 public class HungerGamesEventList {
@@ -19,21 +21,10 @@ public class HungerGamesEventList {
     private static int eventIndex = 0;
 
     public static List<HungerGamesEvent> events() {
-        return List.of(
-                new HungerGamesEvent("Tournament will start in 2 minutes", MIN, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 1 minutes", 30 * SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 30 seconds", 15 * SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 15 seconds", 5 * SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 10 seconds", SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 9 seconds", SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 8 seconds", SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 7 seconds", SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 6 seconds", SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 5 seconds", SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 4 seconds", SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 3 seconds", SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 2 seconds", SEC, null, eventIndex++),
-                new HungerGamesEvent("Tournament will start in 1 seconds", SEC, null, eventIndex++),
+        eventIndex = 0;
+        phase = 0;
+        List<HungerGamesEvent> events = new ArrayList<>(preGameCountdownEvents());
+        events.addAll(List.of(
                 new HungerGamesEvent(String.format("%sLET THE TOURNAMENT BEGIN! GOODLUCK EVERYONE%s", ChatColor.AQUA, ChatColor.RESET), 0, new TournamentStartedEvent(), eventIndex++),
                 new HungerGamesEvent("The world border is " + borderBoundaries(borderRadiuses.get(phase)) + " it will shrink in 7 minutes", MIN, null, eventIndex++),
                 new HungerGamesEvent("Invincibility will wear off in 1 minute", 30 * SEC, null, eventIndex++),
@@ -88,7 +79,56 @@ public class HungerGamesEventList {
                 new HungerGamesEvent("The border is now shrinking to " + borderBoundaries(borderRadiuses.get(phase)) + " over the next 2 minutes", 3 * MIN, new BorderShrinkEvent(borderRadiuses.get(phase++)), eventIndex++),
                 new HungerGamesEvent("Somebody better win in the next minute or tourneys over.", MIN, null, eventIndex++),
                 new HungerGamesEvent("ggs.", MIN, new HungerGamesCompletedEvent(), eventIndex++)
-        );
+        ));
+        return List.copyOf(events);
+    }
+
+    private static List<HungerGamesEvent> preGameCountdownEvents() {
+        List<HungerGamesEvent> preGameCountdownEvents = new ArrayList<>();
+        VanillaHungerGamesConfig config = getPlugin().vanillaHungerGamesConfig();
+        int minutes = config.preGameCountdownMinutes();
+        int seconds = config.preGameCountdownSeconds();
+        if (minutes < 1) {
+            if (seconds > 30) {
+                preGameCountdownEvents.add(new HungerGamesEvent("Tournament will start in " + seconds + " seconds", (seconds - 30) * SEC, null, eventIndex++));
+                preGameCountdownEvents.add(new HungerGamesEvent("Tournament will start in 30 seconds", 15 * SEC, null, eventIndex++));
+                preGameCountdownEvents.add(new HungerGamesEvent("Tournament will start in 15 seconds", 5 * SEC, null, eventIndex++));
+                preGameCountdownEvents.addAll(preGameCountdownEventsSeparatedByASecond(10));
+            } else if (seconds > 15) {
+                preGameCountdownEvents.add(new HungerGamesEvent("Tournament will start in " + seconds + " seconds", (seconds - 15) * SEC, null, eventIndex++));
+                preGameCountdownEvents.add(new HungerGamesEvent("Tournament will start in 15 seconds", 5 * SEC, null, eventIndex++));
+                preGameCountdownEvents.addAll(preGameCountdownEventsSeparatedByASecond(10));
+            } else {
+                preGameCountdownEvents.addAll(preGameCountdownEventsSeparatedByASecond(seconds));
+            }
+            return preGameCountdownEvents;
+        }
+        if (seconds != 0) {
+            preGameCountdownEvents.add(new HungerGamesEvent(String.format("Tournament will start in %s minutes and %s seconds", minutes, seconds), seconds * SEC, null, eventIndex++));
+        }
+        preGameCountdownEvents.addAll(preGameCountdownEventsSeparatedByAMinute(minutes));
+        preGameCountdownEvents.add(new HungerGamesEvent("Tournament will start in 1 minutes", 30 * SEC, null, eventIndex++));
+        preGameCountdownEvents.add(new HungerGamesEvent("Tournament will start in 30 seconds", 15 * SEC, null, eventIndex++));
+        preGameCountdownEvents.add(new HungerGamesEvent("Tournament will start in 15 seconds", 5 * SEC, null, eventIndex++));
+        preGameCountdownEvents.addAll(preGameCountdownEventsSeparatedByASecond(10));
+        return preGameCountdownEvents;
+    }
+
+    private static List<HungerGamesEvent> preGameCountdownEventsSeparatedByASecond(int startingSecond) {
+        List<HungerGamesEvent> events = new ArrayList<>();
+        for (int i = startingSecond; i > 0; i--) {
+            events.add(new HungerGamesEvent(String.format("Tournament will start in %s second%s", i, i == 1 ? "" : "s"), SEC, null, eventIndex++));
+        }
+        return events;
+    }
+
+    /** This excludes the final 1 minute warning event so that the time between it and the next event can be better refined. */
+    private static List<HungerGamesEvent> preGameCountdownEventsSeparatedByAMinute(int startingMinute) {
+        List<HungerGamesEvent> events = new ArrayList<>();
+        for (int i = startingMinute; i > 1; i--) {
+            events.add(new HungerGamesEvent(String.format("Tournament will start in %s minutes", i), MIN, null, eventIndex++));
+        }
+        return events;
     }
 
     public static Integer beginningBorderSize() {
